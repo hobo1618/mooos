74beccf9d07741d0a7b4720c42fb76150a47b406
Hobo Spaceship <hobdenw@gmail.com>
2023-09-18 22:14:09 +0200

    impl custom_test_frameworks

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 mod vga_buffer;
 
 use core::panic::PanicInfo;
@@ -8,6 +18,10 @@ use core::panic::PanicInfo;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -17,3 +31,11 @@ fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1,1);
+    println!("[ok]");
+}
+
