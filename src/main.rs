3fbdb26ea50b8e61cfc196b5f68757feb89b97ca
Hobo Spaceship <hobdenw@gmail.com>
2023-09-19 11:40:20 +0200

    impl Testable trait for test_runner

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use core::panic::PanicInfo;
+
 mod vga_buffer;
 mod serial;
 
@@ -13,6 +15,7 @@ pub enum QemuExitCode {
     Success = 0x10,
     Failed = 0x11,
 }
+
 
 pub fn exit_qemu(exit_code: QemuExitCode) {
     use x86_64::instructions::port::Port;
@@ -24,17 +27,29 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
 
     exit_qemu(QemuExitCode::Success)
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
 
-use core::panic::PanicInfo;
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -66,7 +81,5 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(1, 0);
-    serial_println!("[ok]");
+    assert_eq!(1, 1);
 }
