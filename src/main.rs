764136a7b777d478cf6a158a6f1095dc7139a785
Hobo Spaceship <hobdenw@gmail.com>
2023-09-26 22:37:54 +0200

    complete creating the mapping in paging impl

--- src/main.rs
+++ src/main.rs
@@ -4,72 +4,29 @@
 #![test_runner(mooos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use mooos::println;
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use bootloader::{BootInfo, entry_point};
+use mooos::println;
 
 entry_point!(kernel_main);
 
-
-
-// #[no_mangle]
-// pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    // use mooos::memory::active_level_4_table;
-    use mooos::memory::translate_addr;
-    use x86_64::VirtAddr;
+    use mooos::memory;
+    use x86_64::{VirtAddr,structures::paging::Page};
+
 
     println!("Hello World{}", "!");
     mooos::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    let addresses = [
-        // identity mapped vga_buffer
-        0x8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        boot_info.physical_memory_offset,
-    ];
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
-        println!("{:?} -> {:?}", virt, phys);
-    }
-
-
-    // let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
-    //
-    // for (i, entry) in l4_table.iter().enumerate() {
-    //     if !entry.is_unused() {
-    //         println!("L4 Entry {}: {:?}", i, entry);
-    //     }
-    // }
-
-    // uncomment lines below to trigger a stack overflow
-    // fn stack_overflow() {
-    //     stack_overflow(); // for each recursion, the return address is pushed
-    // }
-    // stack_overflow();
-    
-    // doesn't trigger a page fault because this
-    // address has read permissions
-    let ptr = 0x2031b2 as *mut u8;
-    unsafe { let x = *ptr; }
-    println!("read worked");
-
-    // triggers page fault for because no write permissions
-    // unsafe { *ptr = 42; }
-    // println!("write worked");
-    
-    // triggers a page fault because this
-    // address isn't read or writable
-    // let ptr = 0xdeadbeef as *mut u8;
-    
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(200).write_volatile(0x_f021_f077_f065_f04e)};
 
     #[cfg(test)]
     test_main();
